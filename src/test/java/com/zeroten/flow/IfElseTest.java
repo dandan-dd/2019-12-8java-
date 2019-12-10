f82d24be75157e68dde862df7963273e2f0a457b
何文东 <1368814309@qq.com>
2019-12-10 20:04:07 +0800

    吸血鬼数字

--- src/test/java/com/zeroten/flow/IfElseTest.java
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -2,10 +2,35 @@ package com.zeroten.flow;
 
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 public class IfElseTest {
 
     @Test
     public void testIfElse() {
+        int[] aa=new int[4];
+        int[] bb=new int[4];
+        for(int i=10;i<=99;i++){
+            for(int j=10;j<=99;j++){
+                int sum=i*j;
+                if(sum<1000||sum>9999){
+                    continue;
+                }
+                aa[0]=i/10;
+                aa[1]=i%10;
+                aa[2]=j/10;
+                aa[3]=j%10;
+                bb[0]=sum/1000;
+                bb[1]=(sum%1000)/100;
+                bb[2]=((sum%1000)%100)/10;
+                bb[3]=sum%10;
+                Arrays.sort(aa);
+                Arrays.sort(bb);
+               if( Arrays.equals(aa,bb)){
+                  System.out.println(i+"*"+j+"="+sum);
+               }
+            }
+        }
     }
 
 }
